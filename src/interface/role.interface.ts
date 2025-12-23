584e5f89ec56eef09c60af679c970366fe158a05
wachawich <guy.wpth@gmail.com>
2025-12-23 19:00:18 +0700

    feature[Role] : CRU role feature and alredy create postman api

---
+++ src/interface/role.interface.ts
@@ -0,0 +1,9 @@
+export interface roleFields {
+    role_id?: number
+    role_name?: string
+    role_type?: string
+    access?: JSON[]
+    created_at?: Date
+    updated_at?: Date
+    flag_valid?: boolean
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { getUser } from "../logic/user"
+import { getRole, createRole, updateRole } from "../logic/role"
 
 const router = express.Router();
 
-// User Sys
-router.post("/users.get", getUser);
+// Role Routes
+router.post("/role.get", getRole);
+router.post("/role.create", createRole);
+router.post("/role.update", updateRole);
 
 export default router;
